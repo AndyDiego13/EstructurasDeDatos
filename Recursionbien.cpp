dcafc26262619d36f4554d0e9f830768bc5472bf
AndyDiego13 <andyserrano_d@outlook.com>
2021-08-19 16:53:07 -0500

    Recursion

--- Recursionbien.cpp
+++ Recursionbien.cpp
@@ -7,10 +7,13 @@ Ejercicios de Recursividad
 */
 
 #include <iostream>
+#include <string>
+#include <regex>
 
 using namespace std;
 
 int suma_digitos(int);
+bool palindromo(string cadena, unsigned long izquierda, unsigned int derecha);
 
 int main(int argc, const char** argv)
 {
@@ -22,6 +25,36 @@ int main(int argc, const char** argv)
 
     /* Sumar los dígitos del número e imprimir el resultado */
     std::cout << "La suma de los dígitos es = " << suma_digitos(numero) << std::endl;
+
+    string cadena;
+
+    std::cout << "Entra una cadena de caracteres: " << std::endl;
+    getline(cin, cadena);
+    std::cout << "Cadena: " << cadena << std::endl;
+
+    /*Eliminar los espacios en blanco */
+    auto cadena_sin_espacios = regex_replace(cadena, regex("\\s+"), "");
+
+    /* Invocar a la función recursiva */
+    bool es_palindromo = palindromo(cadena_sin_espacios, 0, cadena_sin_espacios.length()- 1);
+
+    /* mostrar resultado */
+    if (es_palindromo)
+    {
+        std::cout << "La frase es un palíndromo." << std::endl;
+    }
+    else
+    {
+        std::cout << "La frase no es palíndromo." << std::endl;
+    }
+
+    std::cout << std::endl;
+
+
+
+
+
+
     return 0;
 }
 
@@ -60,4 +93,21 @@ int suma_digitos(int n)
     {
         return resto + suma_digitos(n);  
     }  
+}
+
+bool palindromo(string cadena, unsigned long izquierda, unsigned int derecha)
+{
+    if (izquierda >= derecha)
+    {
+        return true;
+    }
+    else if (cadena[izquierda] != cadena[derecha])
+    {
+        return false;
+    }
+    else
+    {
+        return palindromo(cadena, izquierda + 1, derecha - 1);
+    }
+    
 }
