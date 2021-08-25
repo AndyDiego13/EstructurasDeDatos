4992b0f69a70047eaa20f8ae4d96d1c5893594ba
AndyDiego13 <andyserrano_d@outlook.com>
2021-08-25 11:13:59 -0500

    update

--- sumaRecursiva_Tarea_1_diego.cpp
+++ sumaRecursiva_Tarea_1_diego.cpp
@@ -11,42 +11,31 @@
 
 /* La complejidad es lineal, debido a que se está presentando en un ciclo */
 #include<iostream>
-//#include<conio.h>
+
 
 using namespace std;
 
-//Esta función es la que va a realizar la suma recursiva, solo tiene un parametro entero "n" regresa el resultado siendo evaluado por dos condicioneles if
-long int sumaRecursiva (int n){
+//Esta función es la que va a realizar la suma recursiva, solo tiene un parametro entero "n" regresa el resultado recursivo siendo evaluado por dos condicioneles if
+int sumaRecursiva(int n){
 
-    cout << " -----------------------------------------------------------------" << endl;
-    cout << "La sumatoria de " << n << " es: " << endl;
+    if (n == 1){
 
-    /* Condición de Parada */
-    if (n == 0){  
-        n = 0;
+        return 1;
     }
-
-    /* Paso Recursivo */
     else{
-        n= n*(n+1)/2;
-    
-    }
 
-    return n;
+        return n + sumaRecursiva(n-1);
+    }
 }
 
+
 int main (){
-    cout << sumaRecursiva(40) << endl;
-    cout << " -----------------------------------------------------------------" << endl;
-    
-    cout << sumaRecursiva(100) << endl;
-    cout << " -----------------------------------------------------------------" << endl;
 
-    cout << sumaRecursiva(230) << endl;
-    cout << " -----------------------------------------------------------------" << endl;
+   int num;
 
-    cout << sumaRecursiva(340) << endl;
-    cout << " -----------------------------------------------------------------" << endl;
+   std::cout << "Favor ingresar el número: " << std::endl;
+   cin >> num;
 
+   std::cout << "La suma Recursiva es: " << sumaRecursiva(num) << std::endl;
     return 0;
 }
