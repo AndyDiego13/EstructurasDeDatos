d06b1cb0834ebcc2434f03bc55ba2e676677b1b6
AndyDiego13 <andyserrano_d@outlook.com>
2021-10-05 19:16:23 -0500

    save

---
+++ Reto2/ConexionesAux.hpp
@@ -0,0 +1,41 @@
+/*
+    Situacion Problema: Ataques Cibernéticos
+    Created by:  
+    - Serrano Diego Andrea (A01028728)
+    - García Puebla Diego fernando (A01028597)
+    
+    Fecha de realizacion: 05 / 10 /2021
+
+*/
+
+#ifndef ConexionesAux_hpp
+#define ConexionesAux_hpp
+
+#include <iostream>
+#include <string>
+
+class ConexionesAux //Clase auxiliar (para las conexiones entre computadoras)
+{
+
+    public:
+    std::string ip;
+    int puerto;
+    std::string conex;
+
+    ConexionesAux(std::string ip, int puerto, std::string conex)
+    {
+        this -> ip = ip;
+        this -> puerto = puerto;
+        this -> conex = conex;
+    }
+    ~ConexionesAux() {};
+
+    std::string getIp();
+};
+
+std::string ConexionesAux::getIp()
+{
+    return ip;
+}
+
+#endif // !ConexionesAux_hpp
