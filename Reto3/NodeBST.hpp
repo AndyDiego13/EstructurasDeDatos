80181261cd625ddb9052318ecad9f1d340f3d898
AndyDiego13 <andyserrano_d@outlook.com>
2021-10-26 20:41:12 -0500

    new reto3

--- Reto3/NodeBST.hpp
+++ Reto3/NodeBST.hpp
@@ -30,6 +30,20 @@ class NodeBST
         NodeBST(std::string name, int num, NodeBST *_left, NodeBST *_right);
         NodeBST(std::pair<std::string, int> val, NodeBST *_left, NodeBST *_right);
 
+        std::pair<std::string, int> getVal();
+        std::string getValName();
+        int getValNum();
+
+        NodeBST *getLeft();
+        NodeBST *getRight();
+
+        void setVal(std::pair<std::string, int> val);
+        void setValName(std::string name);
+        void setValNum(int num);
+
+        
+
+
 
 };
 
