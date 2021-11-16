967929545c22273647951e783051b4c9e5ae3b70
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-15 22:03:09 -0600

    creación de clase GraphR4

--- Reto4/GraphR4.hpp
+++ Reto4/GraphR4.hpp
@@ -9,3 +9,50 @@
     Fecha de realizacion: 18 / 10 /2021
 
 */
+
+#ifndef GraphR4_hpp
+#define GraphR4_hpp
+
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <list>
+#include <vector>
+#include <algorithm>
+
+template <class T>
+class VertexG
+{
+    private:
+        T val;
+        std::vector<int> adj;
+    
+    public:
+
+        ~VertexG() {};
+        VertexG() {};
+        VertexG(T _val)
+        {
+            val = _val;
+        }
+
+        T getVal()
+        {
+            return val;
+        }
+
+        void setVal(T _val)
+        {
+            val = _val;
+        }
+
+        std::vector<int> getAdj()
+        {
+            return adj;
+        }
+
+        
+
+};
+
+#endif // !GraphR4_hpp
