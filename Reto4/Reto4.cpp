af56f8135ca509a12670e4355b1d11b0bd041727
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-17 18:59:41 -0600

    se agregaron funciones y se comenzo el main

--- Reto4/Reto4.cpp
+++ Reto4/Reto4.cpp
@@ -84,6 +84,28 @@ bool isInterna(std::string ipInput)
     return true;
 }
 
+void fillCompu( std::map< std::string, ConexionesComputadora> &computadoras, std::vector<UserFila> datos)
+{
+    for (size_t i = 0; i < datos.size(); i++)
+    {
+        /* Continuar si la ip origen no esta en el map*/
+        if ( datos[i].ipOrigen != "-" && computadoras.find(datos[i].ipOrigen))
+        {
+            ConexionesComputadora cc(datos[i].ipOrigen, datos[i].nombreOrigen);
+            cc.fill(datos);
+            computadoras.insert(std::pair< std::string, ConexionesComputadora> (datos[i].ipOrigen,cc));
+        }
+        //Continuar si la ip destino no esta en el map
+        if ( datos[i].ipDestino != "-" && computadoras.find(datos[i].ipDestino) == computadoras.end())
+        {
+            ConexionesComputadora cc(datos[i].ipDestino, datos[i].nombreDestino);
+            cc.fill(datos);
+            computadoras.insert( std::pair< std::string, ConexionesComputadora> (datos[i].ipDestino, cc));
+        }
+    }
+}
+
+
 void conexionesDiariasGrafos( std::map<Date, Graph<std::string>> &grafosPorDia, std::set<Date> allDates, std::string ipA, std::map<Date, int> &conexionesEntrantesPorDia, std::map<Date, int> &conexionesSalientesPorDia, std::map<std::string, ConexionesComputadora> todasLasComputadoras)
 {
     //Iteramos por día y creamos el grafo
@@ -112,18 +134,113 @@ void conexionesDiariasGrafos( std::map<Date, Graph<std::string>> &grafosPorDia, 
             for ( list<ConexionesAux>::iterator conexion = iConexionesSalientes.begin(); conexion != iConexionesSalientes.end(); ++conexion)
             {
                 // Si es interna, agregamos conexion
-                if (isInterna(conexion->ip))
+                if (isInterna(conexion->ip) && (conexion->fechaTm == *hoy))
                 {
-                    /* code */
-                }
-                
+                    grafoI.addEdgeElement(iIp, conexion->ip);
 
+                    if (conexion->ip == ipA)
+                    {
+                        conexionesEntrantesPorDia[*hoy]++;
+                    }
+                    if (iIp == ipA)
+                    {
+                        conexionesSalientesPorDia[*hoy]++;
+                    }
+                }
             }
-            
         }
-        
-        
+        grafosPorDia[*hoy] = grafoI; 
     }
-    
 }
 
+/* Obtener Ip con más conexiones por día*/
+std::map< Date, std::string> maxConexionesPorDia( std::map< Date, Graph< std::string>> grafosPorDia)
+{
+    std::map< Date, std::string> maxConexionesPorDia;
+
+    for ( std::map< Date, Graph< std::string>>::iterator it = grafosPorDia.begin(); it != grafosPorDia.end(); ++it)
+    {
+        Graph< std::string> grafoHoy = it->second;
+
+        std::map< std::string, int> numeroConex = grafoHoy.saveVeci();
+        std::string ipMaxConex = "";
+        int maxConex = 0;
+
+        //Iteramos ips en un mapa con la cantidad de ocurrencias diarias
+        for ( std::map< std::string, int>::iterator ips = numeroConex.begin(); ips != numeroConex.end(); ++ips) 
+        {
+            if (ips->second >= maxConex)
+            {
+                ipMaxConex = ips->first;
+                maxConex = ips->second;
+            }
+        }
+        maxConexionesPorDia[it->first] = ipMaxConex; 
+    }
+    return maxConexionesPorDia;
+}
+
+void vertexMaxConexSalientes( std::map< Date, std::string> maxConexionesPorDia, std::string ipA)
+{
+    for ( std::map< Date, std::string>::iterator it = maxConexionesPorDia.begin(); it != maxConexionesPorDia.end(); ++it)
+    {
+        Date hoy = it->first;
+        std::cout << hoy.toString() << ":\t" << (it->second == ipA ? "Si" : "No") << std::endl;
+    } 
+}
+
+bool web(ConexionesAux ca)
+{
+    if (ca.puerto == 443)
+    {
+        return true;
+    }
+    return false;
+}
+
+void conexWebGrafo( std::map< Date, Graph< std::string>> &grafosPorDiaWeb, std::set<Date> allDates, std::string ipI, std::map< Date, int> &conexEntrantesPorDiaWeb, std::map< std::string, ConexionesComputadora> allCompu)
+{
+    for ( std::set<Date>::iterator hoy = allDates.begin(); hoy != allDates.end(); ++hoy)
+    {
+        Graph< std::string> grafoI;
+        conexEntrantesPorDiaWeb[*hoy] = 0;
+
+        //iteramos todas las computadoras para añadir los nodos
+        for ( std::map< std::string, ConexionesComputadora>::iterator compu = allCompu.begin(); compu != allCompu.end(); ++compu)
+        {
+            grafoI.addNode(compu->first);
+        }
+        //Iteramos todas las computadoras para añadir los edges
+        for ( std::map< std::string, ConexionesComputadora>::iterator compu = allCompu.begin(); compu != allCompu.end(); ++compu)
+        {
+            std::string iIp = compu->first;
+            list<ConexionesAux> iConexSalientes = compu->second.conexionesSalientes;
+
+            //Iteramos todas las conexiones salientes y si es web se agrega a conexion
+            for ( list<ConexionesAux>::iterator conexion = iConexSalientes.begin(); conexion != iConexSalientes.end(); ++conexion)
+            {
+                if ( web(*conexion) && (conexion->fechaTm == *hoy))
+                {
+                    grafoI.addEdgeElement(iIp, conexion->ip);
+
+                    if (conexion->ip == ipI)
+                    {
+                        conexEntrantesPorDiaWeb[*hoy]++;
+                    }
+                }
+            }
+        }
+        grafosPorDiaWeb[*hoy] = grafoI;
+    }
+}
+
+int main()
+{
+    Administrador ad;
+    std::vector< UserFila> datos = ad.read_fila();
+    std::map< std::string, ConexionesComputadora> allCompu;
+    fillCompu(allCompu, datos);
+
+    
+    return 0;
+}
