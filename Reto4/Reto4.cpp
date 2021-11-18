700a2accad2f029c7ff0cdabc1805b196da3ff1e
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-18 14:19:58 -0600

    cambios en el main

--- Reto4/Reto4.cpp
+++ Reto4/Reto4.cpp
@@ -377,8 +377,48 @@ int main()
     ConexionesComputadora A = allCompu[ipInternaA];
 
     // Sitio con nombre anómalo
-    std::string ipAnomalaB = foundAnomalo(allCompu);
-    ConexionesComputadora B = allCompu[ipAnomalaB];
+    //std::string ipAnomalaB = foundAnomalo(allCompu);
+    //ConexionesComputadora B = allCompu[ipAnomalaB]; //1.100.63.176 creo que tiene que ser el nombre
+    //esta ip es la unica rara interna 
+    // fecha: 19-8-2020, hora:10:15:34, ipOrigen:1.100.63.176, puertoOrigen:5509, no tiene nombreOrigen -, ipdestino 172.22.162.212, puertoDestino 443, nombreDestino server.reto.com
+    //std::cout << ipAnomalaB << std::endl;
+    std::cout << "\t" << (foundAnomalo(allCompu) != "" ? "Sí." : "No.") << std::endl;
+
+    //Sitio con mucho tráfico en un dia 
+    std::string ipTraficoC = "139.242.229.104"; //freemailserver
+    ConexionesComputadora C = allCompu[ipTraficoC];
+
+    /* 1. Utilizando un grafo con las conexiones entre las ip de la red interna, 
+    determina la cantidad de computadoras con las que se ha conectado A por día. 
+    ¿Es el vértice que más conexiones salientes tiene hacia la red interna? 
+    */
+
+    std::map< Date, Graph< std::string>> grafosPorDia;
+    std::set<Date> allDates = takeDates(datos);
+    std::map< Date, int> conexionesEntratesPorDia;
+    std::map< Date, int> conexionesSalientesPorDia;
+
+    conexionesDiariasGrafos(grafosPorDia, allDates, ipInternaA, conexionesEntratesPorDia, conexionesSalientesPorDia, allCompu);
+
+    std::cout << "1. Utilizando un grafo con las conexiones entre las ip de la red interna, determina la cantidad de computadoras con las que se ha conectado A por día." << std::endl;
+    for ( std::map<Date, int>::iterator it = conexionesSalientesPorDia.begin(); it != conexionesSalientesPorDia.end(); ++it)
+    {
+        Date hoy = it->first;
+        std::cout << hoy.toString() << ":\t" << it->second << std::endl;
+    }
+    
+    std::cout << "¿Es el vértice que más conexiones salientes tiene hacia la red interna?" << std::endl;
+    std::map<Date, std::string>
+
+
+
+    for ( std::map< Date, int>::iterator it = conexionesEntrantesPorDia.begin(); it != conexionesEntrantesPorDia.end(); ++it)
+    {
+        Date hoy = it->first;
+        std::cout << hoy.toString() << ":\t" << it->second << std::endl;
+    }
+    std::cout << "Sí existen conexiones entrantes a la computadora con ip: " << ipInternaA << std::endl;
+    
 
 
 
