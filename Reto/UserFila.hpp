1a35ff47b8c835c28b1a262d1609598b5375cd3a
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-21 17:51:43 -0600

    userfila actualizado

--- Reto/UserFila.hpp
+++ Reto/UserFila.hpp
@@ -20,127 +20,41 @@
 
 using namespace std;
 
-class UserFila {
-public:
-    string fecha;
-    string hora;
-    string ipOrigen;
-    string puertoOrigen;
-    string nombreOrigen;
-    string ipDestino;
-    string puertoDestino;
-    string nombreDestino;
-    tm fechaTm;
-    std::string fechaString;
-    
+class UserFila
+{
+    public:
 
-    int origenPuerto;
-    int destinoPuerto;
-    
-    //Constructor
-    UserFila(string f, string h, string iO, string pO, string nO, string iD, string pD, string nD);
+        UserFila(std::string fecha,
+                 std::string hora,
+                 std::string ipOrigen,
+                 std::string puertoOrigen,
+                 std::string nombreOrigen,
+                 std::string ipDestino,
+                 std::string puertoDestino,
+                 std::string nombreDestino
+        )
 
-    //UserFila(){}
-    
-    //destructor
-    ~UserFila(){}
-    
-    //sobrecarga de operador para imrimir y poder ser ocupado en otras clases
-    friend std::ostream & operator<<(std::ostream & os, const UserFila & userFila)
-    {
-        
-        os << userFila.fecha << " - ";
-        os << userFila.hora << " - ";
-        os << userFila.ipOrigen << " - ";
-        os << userFila.puertoOrigen << " - ";
-        os << userFila.nombreOrigen << " - ";
-        os << userFila.ipDestino << " - ";
-        os << userFila.puertoDestino << " - ";
-        os << userFila.nombreDestino << " - ";
-        os << endl;
-    
-    
-        return os;
+        UserFila();
 
-    }
-           
-    //Obtener Fecha
-    string getFecha()
-    {
-        return fecha;
-    }
+        tm fecha;
+        std::string fechaString;
+        std::string hora;
+        std::string ipOrigen;
+        int puertoOrigen;
+        std::string nombreOrigen;
+        std::string ipDestino;
+        int puertoDestino;
+        std::string nombreDestino;
 
-    //Obtener Hora
-    string getHora()
-    {
-        return hora;
-    }
-
-    //Obtener Ip Origen
-    string getIpO()
-    {
-        return ipOrigen;
-    }
-
-    string getPuertoO()
-    {
-        return puertoOrigen;
-    }
-
-    //Obtener nombre del puerto origen
-    string getNombreO()
-    {
-        return nombreOrigen;
-    }
-
-    //Obtener ip Destino
-    string getIpD()
-    {
-        return ipDestino;
-    }
-
-    //Obtener nombre del puerto destino
-    string getPuertoD()
-    {
-        return puertoDestino;
-    }
-
-    string getNombreD()
-    {
-        return nombreDestino;
-    }
-
-    //para comparar todo por fecha
-
-    static bool fecha_asc(UserFila co, UserFila b)
-    {
-        return co.fecha < b.fecha;
-    }
-
-    void printDate()
-    {
-        std::cout << this->fechaTm.tm_mday << "/" << this->fechaTm.tm_mon +1 << "/" << this->fechaTm.tm_year+1900 <<std::endl;
-    }
-
-    void print()
-    {
-        this->printDate();
-        std::cout << "\t" << this->hora
-        << "\t" << this->ipOrigen
-        << "\t" << this->puertoOrigen
-        << "\t" << this->nombreOrigen
-        << "\t" << this->ipDestino
-        << "\t" << this->puertoDestino
-        << "\t" << this->nombreDestino
-        <<std::endl<< "========================"<<std::endl;
-    }
+        void print();
+        void printDate(); 
 };
 
-UserFila::UserFila(string f, string h, string iO, string pO, string nO, string iD, string pD, string nD)
+UserFila::UserFila(std::string f, std::string h, std::string iO, std::string pO, std::string nO, std::string iD, std::string pD, std::string nD)
 {
     struct tm tm;
-    const char *_fecha = f.c_str();
-    strptime(_fecha, "%d-%m-%y", &this->fechaTm);
+    const char *f = _fecha.c_str();
+    strptime(f, "%d-%m-%y", &this->fecha);
     this->fechaString = _fecha;
     this->hora = h;
     this->ipOrigen = iO;
@@ -160,5 +74,22 @@ UserFila::UserFila(string f, string h, string iO, string pO, string nO, string i
     }
 }
 
+void UserFila::print()
+{
+    this->printDate();
+    std::cout << "\t" << this->hora 
+    << "\t" << this->ipOrigen
+    << "\t" << this->puertoOrigen
+    << "\t" << this->nombreOrigen
+    << "\t" << this->ipDestino
+    << "\t" << this->puertoDestino
+    << "\t" << this->nombreDestino
+    <<std::endl << "=======================" <<std::endl;
+}
+
+void UserFila::printDate()
+{
+    std::cout << this->fecha.tm_mday << "/" << this->fecha.tm_mon + 1 << "/" << this->fecha.tm_year + 1900 << std::endl;
+}
 
 #endif /* UserFila_hpp */
