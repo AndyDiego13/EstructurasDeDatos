9944372732c040cf57a441e56dd6532a787525a9
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-21 18:39:25 -0600

    main

--- Reto/main.cpp
+++ Reto/main.cpp
@@ -9,173 +9,166 @@ Fecha de entrega: 23/09/2021
 #include "UserFila.hpp"
 #include <string>
 #include <iterator>
+#include <map>
 
+std::map< std::string, int> mails;
+std::map<int, int> puertos;
 
-auto  read_csv_strings(std::string filename)
+/* Imprimir vectores */
+void printVector( std::vector<UserFila> arr)
 {
-    /* Crear un vector (matriz) de vector de strings (vectores internos o sea filas) donde se guardara cada linea del documento */
-    vector <vector <std::string>> lineas;
-
-    /* Crear un flujo de archivos de entrada para leer el archivo CSV, o sea objeti que reciba un input */
-    ifstream file(filename);
-
-    /* Lanzar excepción: Verificar si no hubo error al abrir el archivo */
-    if (!file.is_open())throw std::runtime_error ( " No se puede abrir el archivo " );
-
-    /* Definir variable que almacena cada línea del archivo */
-    std::string linea;
-    
-    /* Leer datos línea a línea, fila a fila, ESTO SE REALIZA PARA CADA LINEA */
-    while ( getline (file, linea,','))
+    for (int i = 0; i < arr.size(); i++)
     {
-        /* Crear un stream de la línea */
-       std::stringstream ss (linea);
-    
-        /* Variable que almacena cada campo leído, o sea cada columna creo */
-        std::string campo;
-        
-        /* Cada elemento de la columna se agrega un vector, es decir cada fila se vuelve a un vector. Almacenar cada campo */
-        vector <std::string> fila;
-        
-        /* Iterar sobre la línea para extraer cada campo */
-        while ( std::getline (ss, campo))
+        arr[i].print();
+        std::cout << std::endl;
+    }
+}
+
+/* Búsqueda Secuencial */
+int busquedaSecuencial( std::vector<UserFila> d, bool (*condicion)(UserFila r))
+{
+    for (int i = 0; i < d.size(); i++)
+    {
+        if (condicion(d[i], r))
         {
-            // se agrega cada elemento al vector de cada fila
-            fila.push_back (campo);
+            return i;
         }
-        
-        /* Insertar la fila con los campos separados en un vector */
-        lineas.push_back (fila);
     }
-
-    /* Cerrar el archivo */
-    file.close ();
-
-    /* Regresar el vector de líneas leídas */
-    return lineas;
+    return -1;
 }
 
-            
-auto  read_csv_USERFILA (std::string filename)
+/* Búsqueda Secuencial Sobrecarga */
+int busquedaSecuencial( std::vector<UserFila> d, bool (*condicion)(UserFila a, UserFila b), UserFila r)
 {
-    /* Crear un vector de vector de strings */
-    vector <UserFila> lineas;
-
-    /* Crear un flujo de archivos de entrada para leer el archivo CSV */
-    std::ifstream file(filename);
-
-    /* Verificar si no hubo error al abrir el archivo */
-    if (! file.is_open ()) throw  std::runtime_error ( " No se puede abrir el archivo " );
-
-    /* Definir variable que almacena cada línea */
-    std :: string linea;
-    
-    /* Leer datos línea a línea */
-    while ( std::getline (file, linea))
+    for (int i = 0; i < d.size(); i++)
     {
-        /* Crear un vapor de la línea */
-        stringstream ss (linea);
-    
-        /*  Variables que almacenan cada campo leído */
-        std::string f;
-        std::string h;
-        std::string iO;
-        std::string pO;
-        std::string nO;
-        std::string iD;
-        std::string pD;
-        std::string nD;
-        
-        /* Iterar sobre la línea para extraer cada campo */
-        getline (ss, f, ',');
-        getline (ss, h, ',');
-        getline (ss, iO, ',');
-        getline (ss, pO,',');
-        getline (ss, nO,',');
-        getline (ss, iD,',');
-        getline (ss, pD,',' );
-        getline (ss, nD,',');
-        
-        
-        /* Insertar la fila con los campos separados en un vector */
-        lineas.push_back ( UserFila (f, h, iO, pO, nO, iD, pD, nD));
+        if (condicion(d[i], r))
+        {
+            return i;
+        }
     }
-
-    /* Cerrar el archivo */
-    file.close ();
-
-    /* Regresar el vector de líneas leídas */
-    return lineas;
+    return -1;
 }
 
+/* Búsqueda Binaria */
+int busquedaBinaria( std::vector<UserFila> d, bool (*condicion)(UserFila r), int inicio, int final)
+{
+    int med = inicio + (final - inicio)/2;
+
+    if (final <= inicio)
+    {
+        return -1;
+    }
+    if (condicion(d[med]))
+    {
+        return med;
+    }
+    int izq = busquedaBinaria(d, *condicion, inicio, med-1);
+
+    if (izq != -1)
+    {
+        return izq;
+    }
+
+    int der = busquedaBinaria(d, *condicion, med+1, final);
+
+    if (der != -1)
+    {
+        return der;
+    }
+    
+    return -1;
+}
+
+bool esPosterior(UserFila a, UserFila b)
+{
+    return (
+        a.fecha.tm_mday > b.fecha.tm_mday &&
+        a.fecha.tm_mon >= b.fecha.tm_mon &&
+        a.fecha.tm_year >= b.fecha.tm_year
+    );
+}
+
+bool perteneceA(UserFila r)
+{
+    return(
+        r.nombreOrigen == "amy.reto.com" ||
+        r.nombreOrigen == "anna.reto.com" ||
+        r.nombreOrigen == "betty.reto.com" ||
+        r.nombreOrigen == "catherine.reto.com" ||
+        r.nombreOrigen == "charles.reto.com" ||
+        r.nombreOrigen == "christopher.reto.com" ||
+        r.nombreOrigen == "cynthia.reto.com" ||
+        r.nombreOrigen == "david.reto.com" ||
+        r.nombreOrigen == "dennis.reto.com" ||
+        r.nombreOrigen == "emma.reto.com" ||
+        r.nombreOrigen == "george.reto.com" ||
+        r.nombreOrigen == "jack.reto.com" ||
+        r.nombreOrigen == "james.reto.com" ||
+        r.nombreOrigen == "janet.reto.com" ||
+        r.nombreOrigen == "jason.reto.com" ||
+        r.nombreOrigen == "jerry.reto.com" ||
+        r.nombreOrigen == "kathleen.reto.com" ||
+        r.nombreOrigen == "kevin.reto.com" ||
+        r.nombreOrigen == "kimberly.reto.com" ||
+        r.nombreOrigen == "mark.reto.com" ||
+        r.nombreOrigen == "michelle.reto.com" ||
+        r.nombreOrigen == "nicole.reto.com" ||
+        r.nombreOrigen == "rachel.reto.com" ||
+        r.nombreOrigen == "raymond.reto.com" ||
+        r.nombreOrigen == "scott.reto.com" ||
+        r.nombreOrigen == "stephanie.reto.com"
+
+    );
+}
+
+bool callServer(UserFila r)
+{
+    return r.nombreOrigen == "server.reto.com" || r.nombreDestino == "server.com";
+}
+
+bool correo(UserFila r)
+{
+    if (r.puertoDestino == 993)
+    {
+        mails[r.nombreDestino]++;
+    }
+    return false;
+}
+
+bool puerto(UserFila r)
+{
+    if (r.puertoDestino < 1000 && r.puertoDestino > 0)
+    {
+        puertos[r.puertoDestino]++;
+    }
+    return false;
+}
+
+std::string obtenerIpBase( std::vector<UserFila> d)
+{
+    int i = busquedaSecuencial(d, [](UserFila r) {return r.ipOrigen != "-";});
+    std::string ip = d[i+1].ipOrigen;
+
+    for (int i = 0; i < 3; i++)
+    {
+        ip.pop_back();
+    }
+    
+    ip.append(".0");
+    return ip;
+}
 
 int main()
 {
+    Administrador admin;
+    std::vector<UserFila> datos = admin.file();
+
+    std::cout << "1. ¿Cuántos registros tiene tu archivo?" << std::endl
+    << "El archivo tiene " << datos.size() << " registros." << endl << endl;
+
+    std::cout << "2. " << std::endl;
    
-
-    Administrador admin ( read_csv_USERFILA ( "/Users/andydiego13/Downloads/Requipo7.csv" ));
-    
-    vector <UserFila> registros = read_csv_USERFILA (  "/Users/andydiego13/Downloads/Requipo7.csv" );
-
-    //Pregunta 1
-    admin.contarRegistros();
-
-    //Pregunta 2 aparece 0
-    std::cout << "Pregunta 2: ¿Cuantos registros hay en el segundo dia?" << std::endl;
-    std::cout << admin.contarXdia("11-8-2020") << std::endl;
-
-    /*Pregunta 3*/
-    std::cout << "Pregunta 3: ¿Alguna de las computadoras pertenece a Jeffrey, Betty, Katherine, Scott, Benjamin, Samuel o Raymond?" << std::endl;
-
-    std::cout << "Busqueda para Jeffrey" << std::endl;
-    admin.busqNombre("jeffrey");
-
-    std::cout << "Busqueda para Betty" << std::endl;
-    admin.busqNombre("betty");
-
-    std::cout << "Busqueda para Katherine" << std::endl;
-    admin.busqNombre("katherine");
-
-    std::cout << "Busqueda para Scott" << std::endl;
-    admin.busqNombre("scott");
-
-    std::cout << "Busqueda para Benjamin" << std::endl;
-    admin.busqNombre("benjamin");
-
-    std::cout << "Busqueda para Samuel" << std::endl;
-    admin.busqNombre("samuel");
-
-    std::cout << "Busqueda para Raymond" << std::endl;
-    admin.busqNombre("raymond");
-    
-
-    /*Pregunta 4*/
-
-    std::cout << "Pregunta 4: ¿Cuál es la dirección de la red interna de la compañía?" << std::endl;
-    admin.busqRedInterna();
-    admin.busqNombreIp(registros);
-
-    //Pregunta 5
-
-    std::cout << "Pregunta 5: Busqueda para computadora: server.reto.com" << std::endl;
-    admin.busqNombre("server");
-
-    //Pregunta 6
-    std::cout << "Pregunta 6: ¿Qué servicio de correo electrónico utilizan (algunas ideas: Gmail, Hotmail, Outlook, Protonmail)?" << std::endl;
-    
-    admin.mostrarMail();
-    admin.busqNombreMail("freemailserver");
-    admin.busqNombreMail("gmail");
-    admin.busqNombreMail("hotmail");
-    admin.busqNombreMail("outlook");
-    admin.busqNombreMail("protonmail");
-    admin.busqNombreMail("mail.yahoo");
-    
-    
-
-    //Pregunta 7
-    std::cout << "Pregunta 6: Considerando solamente los puertos destino ¿Qué puertos abajo del 1000 se están usando? Lista los puertos e investiga qué aplicación/servicio lo utiliza generalmente." << std::endl;
-    admin.mostrarRangoPuertosD(0,1000);
 
    
     
