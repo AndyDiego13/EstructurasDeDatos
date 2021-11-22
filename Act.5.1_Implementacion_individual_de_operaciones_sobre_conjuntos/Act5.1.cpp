6472cbbd088997e7e0258d8c6a619a1221cb1862
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-22 07:37:27 -0600

    se creo act5.1 hashmap

---
+++ Act.5.1_Implementacion_individual_de_operaciones_sobre_conjuntos/Act5.1.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include "HashMap.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    int capacity = 5;
+    
+    /* Crear un HashMap */
+    HashMap<int, int> * tabla = new HashMap<int, int>(capacity);
+    
+    /* Insertar elementos */
+    std::cout << tabla->put(1, 1) << std::endl;
+    std::cout << tabla->put(5, 5) << std::endl;
+    std::cout << tabla->put(11, 11) << std::endl;
+    std::cout << tabla->put(15, 15) << std::endl;
+    std::cout << tabla->put(2, 2) << std::endl;
+    std::cout << tabla->put(8, 8) << std::endl;
+    
+    std::cout << tabla->get(5) << std::endl;
+    std::cout << tabla->get(15) << std::endl;
+    std::cout << tabla->get(8) << std::endl;
+    
+    std::cout << *tabla;
+    
+    /* Eliminar la tabla */
+    delete tabla;
+    
+    return 0;
+}
