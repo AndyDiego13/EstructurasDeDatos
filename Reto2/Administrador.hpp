e42f64b4c7e4c5e62e12683cf6e4374b59d2943d
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-21 20:20:32 -0600

    cambio de Administrador

--- Reto2/Administrador.hpp
+++ Reto2/Administrador.hpp
@@ -10,6 +10,7 @@
 
 */
 
+
 #ifndef Administrador_hpp
 #define Administrador_hpp
 
@@ -26,202 +27,51 @@
 
 using namespace std;
 
-class Administrador{
-    private:
-        string fileName;
-        vector<UserFila> registros;
-        
+class Administrador
+{
     public:
-        long int size = registros.size();
-        Administrador(vector<UserFila> r): registros(r){};
+
+        
         Administrador(){}
         ~Administrador(){}
 
-        
-        auto read_fila()
+        std::vector<UserFila> file()
         {
-            /* Crear un vector de vector de strings */
-            vector<UserFila> lineas;
+            std::ifstream f("/Users/andydiego13/Downloads/Requipo7.csv");
+            std::vector<UserFila> registros;
 
-            /* Crear un input filestream para leer el archivo CSV */
-            ifstream file(fileName);
-
-            /* Verificar si no hubo error al abrir el archivo */
-            if(!file.is_open()) throw runtime_error("No se puede abrir el archivo");
-
-            /* Definir variable que almacena cada línea */
-            string linea;
-    
-            /* Leer datos línea a línea */
-            while(getline(file, linea))
+            if (!f.is_open())
             {
-                /* Crear un steam de la línea */
-                stringstream ss(linea);
-    
-                /* Variables que almacenan cada campo leído */
-                string f;
-                string h;
-                string iO;
-                string pO;
-                string nO;
-                string iD;
-                string pD;
-                string nD;
-                
-        
-                /* Iterar sobre la línea para extraer cada campo */
-                getline(ss, f, ',');
-                getline(ss, h, ',');
-                getline(ss, iO, ',');
-                getline(ss, pO, ',');
-                getline(ss, nO, ',');
-                getline(ss, iD, ',');
-                //esto no funciona porque yo quiero ingresar un int y getline no se ocupara ints
-                getline(ss, pD, ',');
-                getline(ss, nD, ',');
-                
-        
-        
-                /* Insertar la fila con los campos separados en un vector */
-                lineas.push_back(UserFila( f, h, iO, pO, nO, iD, pD, nD));
+                std::cout << "Error: File Open" << std::endl;
             }
+
+            std::string fecha;
+            std::string hora;
+            std::string ipOrigen;
+            std::string puertoOrigen;
+            std::string nombreOrigen;
+            std::string ipDestino;
+            std::string puertoDestino;
+            std::string nombreDestino;
+
+            while (f.good())
+            {
+                getline(f, fecha, ',');
+                getline(f, hora, ',');
+                getline(f, ipOrigen, ',');
+                getline(f, puertoOrigen, ',');
+                getline(f, nombreOrigen, ',');
+                getline(f, ipDestino, ',');
+                getline(f, puertoDestino, ',');
+                getline(f, nombreDestino, '\n');
+
+                registros.push_back(UserFila(fecha, hora, ipOrigen, puertoOrigen, nombreOrigen, ipDestino, puertoDestino, nombreDestino));
+            }
+
+            f.close();
             
-
-            /* Cerrar el archivo */
-            file.close();
-            
-
-            /* Regresar el vector de líneas leídas */
-            return lineas;
+            return registros;
         }
-        
-        
-
-        int contarRegistros()
-        {
-            //atributo de clase
-    
-            std::cout<< "Numero de registros: " << size << std::endl;
-        }
-
-        int contarXdia(string dia)
-        {
-    
-            int cont = 0;
-    
-            for (int i = 0; i < size; i++){
-                if(registros[i].getFecha() == dia){
-                    cont = cont +1;
-                }
-            }
-    
-            return cont;
-        }
-
-    void busqNombre(string nombre)
-    {
-        int cont = 0;
-    
-        for (int i = 0; i < size; i++){
-            if(registros[i].getNombreO() == nombre+".reto.com" ){
-                //cout << registros[i];
-                cont = cont +1;
-            }
-        }
-        cout << " Valores encontrados : " << cont << " para : "<<  nombre << endl;
-    }
-
-    void busqNombreMail(string nombreDestino)
-    {
-        int cont = 0;
-    
-        for (int i = 0; i < size; i++){
-            if(registros[i].getNombreD() == nombreDestino + ".com" ){
-                //cout << registros[i];
-                cont = cont +1;
-            }
-        }
-        cout << " Valores encontrados : " << cont << " para : "<<  nombreDestino << endl;
-    }
-
-    void busqRedInterna()
-    {
-
-        int i=0;
-        //lo que ocurre es que se voltea la dirección IP de origin y todo lo que está en el útilmo octeto se borra
-
-        while (i<size)
-        {
-            string ip_found=registros[i].getIpO();
-            std::reverse(ip_found.begin(), ip_found.end());
-            int pos = ip_found.find(".");
-            string cut = ip_found.substr(pos + 1);
-            std::reverse(cut.begin(), cut.end());
-            cout<< cut<<endl;
-            i++;
-        }
-    }
-
-    int busquedaSecuencial( vector<UserFila> d, bool (*condicion)(UserFila r) )
-    {
-        for(int i = 0; i<d.size(); i++)
-        {
-            if( condicion(d[i]) ) return i;
-        }
-        return -1;
-    }
-
-    void busqNombreIp(vector<UserFila> d)
-    {
-        int h = busquedaSecuencial(d, [](UserFila r){return r.getIpO() != "-";});
-        std::string iporigen = d[h+1].getIpO();
-        for (int i = 0; i < 3; i++) iporigen.pop_back();
-        iporigen.append(".212");
-        std::cout<< "La ip de la red interna: " << iporigen << std::endl;
-    }
-
-    void mostrarMail(){
-    
-        for (int i = 0; i < size; i++){
-                cout << registros[i].getNombreD() << endl;
-        }
-    
-    }
-    void busqNombreDestino(string nombre){
-    
-        int cont = 0;
-    
-        for (int i = 0; i < size; i++){
-            if(registros[i].getNombreD() == nombre ){
-                //cout << registros[i];
-                cont = cont +1;
-            }
-        }
-        cout << " Valores encontrados : " << cont << " para : "<<  nombre << endl;
-    
-    }
-
-    void mostrarRangoPuertosD(int inf, int sup){
-    
-    
-        int puertoDesINT;
-        int cont = 0;
-
-        for(int j = 0; j < size; j++)
-        {
-            std::cout << registros[j].getPuertoD() << std::endl;
-        }
-
-        for (int i = 0; i < size; i++){
-            puertoDesINT =  atoi( registros[i].getPuertoD().c_str() ) ;
-            if( inf < puertoDesINT && puertoDesINT < sup ){
-                cont = cont + 1;
-            }
-        }
-        cout << " Valores encontrados de los puertos menores a 1000 : " << cont << endl;
-
-    }
-      
 };
 
 #endif /* Administrador_hpp */
