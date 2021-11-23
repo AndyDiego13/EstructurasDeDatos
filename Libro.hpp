3bbc615c08c0135152663e61ce7cee6cda15418e
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-23 12:30:27 -0600

    clase libro para stl map

---
+++ Libro.hpp
@@ -0,0 +1,29 @@
+#ifndef Libro_hpp
+#define Libro_hpp
+
+#include <iostream>
+
+class Libro {
+
+    std::string isbn;
+    std::string titulo;
+    int paginas;
+    
+public:
+    Libro()
+    {
+        Libro("sin definir", "sin definir", 0);
+    }
+    
+    Libro(std::string _isbn, std::string _titulo, int _pags) :
+    titulo(_titulo), isbn(_isbn), paginas(_pags)
+    {
+        
+    }
+    
+    ~Libro() {}
+    
+    friend std::ostream & operator<<(std::ostream &, const Libro &);
+};
+
+#endif /* Libro_hpp */
