8a222a13bb02ad9cd7f6aa7ed1ecd2df858d99c5
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-29 20:36:10 -0600

    cambios

--- Reto2/reto2.5.cpp
+++ Reto2/reto2.5.cpp
@@ -91,7 +91,7 @@ void agregarNoRetoASetyLlenarComputadoras(std::set<std::string> s, std::map<std:
 
 bool esAnomalo(std::string nombre)
 {
-    if (nombre == "jhntee9opzbxvdv2unkx.net")
+    if (nombre.size() >= 20)
     {
         return true;
     }
